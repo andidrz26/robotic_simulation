42bfbd855e41a02e417649569a8c77e7142d3105
Andre Dreitz <andre.dreitz@stud.uni-bamberg.de>
2024-11-26 12:50:45 +0100

    added endpoints
    - for matrix: sum_of
    - for vector: sum_of, cross_product_of and scalar_product_of
    - added all to invokehandler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,14 +5,50 @@ pub mod algorithms {
     pub mod vector;
 }
 
-use algorithms::matrix::product_of;
+use algorithms::matrix::{product_of, sum_of};
+use algorithms::vector;
+use algorithms::vector::{cross_product_of, scalar_product_of};
 
 #[tauri::command(async, rename_all = "snake_case")]
-fn get_multiplied_matrix(first_factor: Vec<Vec<f64>>, second_factor: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
-
+fn get_multiplied_matrix(
+    first_factor: Vec<Vec<f64>>,
+    second_factor: Vec<Vec<f64>>,
+) -> Vec<Vec<f64>> {
     match product_of(first_factor, second_factor) {
         Some(value) => return value,
         None => Vec::new(),
+    }
+}
+
+#[tauri::command(async, rename_all = "snake_case")]
+fn get_added_matrix(first_summand: Vec<Vec<f64>>, second_summand: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+    match sum_of(first_summand, second_summand) {
+        Some(value) => return value,
+        None => Vec::new(),
+    }
+}
+
+#[tauri::command(async, rename_all = "snake_case")]
+fn get_added_vector(first_summand: Vec<f64>, second_summand: Vec<f64>) -> Vec<f64> {
+    match vector::sum_of(first_summand, second_summand) {
+        Some(value) => return value,
+        None => Vec::new(),
+    }
+}
+
+#[tauri::command(async, rename_all = "snake_case")]
+fn get_cross_product(first_factor: [f64; 3], second_factor: [f64; 3]) -> [f64; 3] {
+    match cross_product_of(first_factor, second_factor) {
+        Some(value) => return value,
+        None => [0.0; 3],
+    }
+}
+
+#[tauri::command(async, rename_all = "snake_case")]
+fn get_scalar_product(first_factor: Vec<f64>, second_factor: Vec<f64>) -> f64 {
+    match scalar_product_of(&first_factor, second_factor) {
+        Some(value) => return value,
+        None => 0.0,
     }
 }
 
@@ -20,7 +56,13 @@ fn get_multiplied_matrix(first_factor: Vec<Vec<f64>>, second_factor: Vec<Vec<f64
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![get_multiplied_matrix])
+        .invoke_handler(tauri::generate_handler![
+            get_multiplied_matrix,
+            get_added_matrix,
+            get_added_vector,
+            get_cross_product,
+            get_scalar_product
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
