c529c0526f6b9e00bf2e1eae4ad709026c3aa822
andidrz <andre.dreitz@stud.uni-bamberg.de>
2024-12-12 17:20:08 +0100

    changed from while to for

--- src-tauri/src/algorithms/matrix/matrix_addition.rs
+++ src-tauri/src/algorithms/matrix/matrix_addition.rs
@@ -4,18 +4,13 @@ pub fn sum_of(first_summand: Vec<Vec<f64>>, second_summand: Vec<Vec<f64>>) -> Op
         && first_summand[0].len() == second_summand[0].len()
     {
         let mut sum: Vec<Vec<f64>> = Vec::new();
-
-        let mut i: usize = 0;
         let col_length: usize = first_summand.len();
         let row_length: usize = first_summand[0].len();
-        while i < col_length {
-            let mut j: usize = 0;  
+        for i in 0..col_length {
             sum.push(Vec::new());
-            while j <  row_length {
+            for j in 0..row_length {
                 sum[i].push(first_summand[i][j] + second_summand[i][j]);
-                j += 1;
             }
-            i += 1;
         }
         Some(sum)
     } else {
