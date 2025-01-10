bd043a3e662a076b25fc1129d10c22344faad333
andidrz <andre.dreitz@stud.uni-bamberg.de>
2025-01-10 17:29:47 +0100

    added test cases
    - matrix
    - vector

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,18 +11,18 @@ use algorithms::vector::{cross_product_of, scalar_product_of};
 
 #[derive(Debug, thiserror::Error)]
 enum Error {
-  #[error(transparent)]
-  Io(#[from] std::io::Error)
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
 }
 
 // we must manually implement serde::Serialize
 impl serde::Serialize for Error {
-  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-  where
-    S: serde::ser::Serializer,
-  {
-    serializer.serialize_str(self.to_string().as_ref())
-  }
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
 }
 
 #[tauri::command(async, rename_all = "snake_case")]
@@ -34,11 +34,13 @@ fn get_multiplied_matrix(
         Ok(value) => Ok(value),
         Err(error) => Err(Error::Io(error)),
     }
-    
 }
 
 #[tauri::command(async, rename_all = "snake_case")]
-fn get_added_matrix(first_summand: Vec<Vec<f64>>, second_summand: Vec<Vec<f64>>) -> Result<Vec<Vec<f64>>, Error> {
+fn get_added_matrix(
+    first_summand: Vec<Vec<f64>>,
+    second_summand: Vec<Vec<f64>>,
+) -> Result<Vec<Vec<f64>>, Error> {
     match sum_of(first_summand, second_summand) {
         Ok(value) => Ok(value),
         Err(error) => Err(Error::Io(error)),
@@ -86,33 +88,105 @@ pub fn run() {
 
 #[cfg(test)]
 mod tests {
-    use crate::algorithms::matrix::product_of;
+    use crate::algorithms::{matrix::{product_of, sum_of}, vector};
+    use std::sync::Once;
+
+    static INIT: Once = Once::new();
+
+    pub fn initialize_3x3(first: &mut Vec<Vec<f64>>, second: &mut Vec<Vec<f64>>) {
+        INIT.call_once(|| {
+            first.push(vec![1.0, 2.0, 3.0]);
+            first.push(vec![4.0, 5.0, 6.0]);
+            first.push(vec![7.0, 8.0, 9.0]);
+
+            second.push(vec![7.0, 8.0, 9.0]);
+            second.push(vec![1.0, 2.0, 3.0]);
+            second.push(vec![4.0, 5.0, 6.0]);
+        });
+    }
 
     #[test]
-    fn test_scalar_product() {
-        let first_factor: Vec<Vec<f64>> = vec![
-            vec![1.0, 2.0, 3.0],
-            vec![4.0, 5.0, 6.0],
-            vec![7.0, 8.0, 9.0],
-        ];
-        let second_factor: Vec<Vec<f64>> = vec![
-            vec![7.0, 8.0, 9.0],
-            vec![1.0, 2.0, 3.0],
-            vec![4.0, 5.0, 6.0],
-        ];
+    fn test_matrix_product() {
+        let mut first_factor: Vec<Vec<f64>> = Vec::new();
+        let mut second_factor: Vec<Vec<f64>> = Vec::new();
+        initialize_3x3(&mut first_factor, &mut second_factor);
+
         let asserted_result: Vec<Vec<f64>> = vec![
             vec![21.0, 27.0, 33.0],
             vec![57.0, 72.0, 87.0],
             vec![93.0, 117.0, 141.0],
         ];
-        let actual_result;
         match product_of(first_factor, second_factor) {
-            Ok(result) => actual_result = result,
+            Ok(result) => assert_eq!(result, asserted_result),
             Err(error) => {
-                actual_result = vec![];
-                println!("{}", error);
-            }, 
+                println!("Error: {:?}", error);
+                assert!(true, "Error: {:?}", error);
+            }
         }
-        assert_eq!(actual_result, asserted_result)
+    }
+
+    #[test]
+    fn test_matrix_sum() {
+        let mut first_summand: Vec<Vec<f64>> = Vec::new();
+        let mut second_summand: Vec<Vec<f64>> = Vec::new();
+        initialize_3x3(&mut first_summand, &mut second_summand);
+
+        let asserted_result: Vec<Vec<f64>> = vec![
+            vec![8.0, 10.0, 12.0],
+            vec![5.0, 7.0, 9.0],
+            vec![11.0, 13.0, 15.0],
+        ];
+        match sum_of(first_summand, second_summand) {
+            Ok(result) => assert_eq!(result, asserted_result),
+            Err(error) => {
+                println!("Error: {:?}", error);
+                assert!(true, "Error: {:?}", error);
+            }
+        }
+    }
+
+    #[test]
+    fn test_vector_sum() {
+        let first_factor: Vec<f64> = vec![1.0, 2.0, 3.0];
+        let second_factor: Vec<f64> = vec![7.0, 8.0, 9.0];
+
+        let asserted_result: Vec<f64> = vec![8.0, 10.0, 12.0];
+        match vector::sum_of(first_factor, second_factor) {
+            Ok(result) => assert_eq!(result, asserted_result),
+            Err(error) => {
+                println!("Error: {:?}", error);
+                assert!(true, "Error: {:?}", error);
+            }
+        }
+    }
+
+    #[test]
+    fn test_vector_cross_product() {
+        let first_factor: [f64; 3] = [1.0, 2.0, 3.0];
+        let second_factor: [f64; 3] = [7.0, 8.0, 9.0];
+
+        let asserted_result: [f64; 3] = [-6.0, 12.0, -6.0];
+        match vector::cross_product_of(first_factor, second_factor) {
+            Ok(result) => assert_eq!(result, asserted_result),
+            Err(error) => {
+                println!("Error: {:?}", error);
+                assert!(true, "Error: {:?}", error);
+            }
+        }
+    }
+
+    #[test]
+    fn test_vector_scalar_product() {
+        let first_factor: Vec<f64> = vec![1.0, 2.0, 3.0];
+        let second_factor: Vec<f64> = vec![7.0, 8.0, 9.0];
+
+        let asserted_result: f64 = 50.0;
+        match vector::scalar_product_of(&first_factor, second_factor) {
+            Ok(result) => assert_eq!(result, asserted_result),
+            Err(error) => {
+                println!("Error: {:?}", error);
+                assert!(true, "Error: {:?}", error);
+            }
+        }
     }
 }
