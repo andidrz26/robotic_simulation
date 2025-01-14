7cf3687362ecb52225c70b90d19a5ef704347c9e
andidrz <andre.dreitz@stud.uni-bamberg.de>
2025-01-14 19:14:43 +0100

    update of angular and primeng
    - standalone true

---
+++ src/app/app.config.ts
@@ -0,0 +1,26 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura,
+                options: {
+                    prefix: 'p',
+                    darkModeSelector: 'system',
+                    cssLayer: false
+                }
+            }
+        }),
+        providePrimeNG({
+            ripple: true
+        })
+    ]
+};
