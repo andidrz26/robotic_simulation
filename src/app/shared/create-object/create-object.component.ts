ad8bcbfc478daaf703a4e29fde8fab4aae6990f0
andidrz <andre.dreitz@stud.uni-bamberg.de>
2025-01-18 20:20:36 +0100

    frontend to create simple obj

--- src/app/shared/create-object/create-object.component.ts
+++ src/app/shared/create-object/create-object.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { SelectModule } from 'primeng/select';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-create-object',
   standalone: true,
-  imports: [],
+  imports: [ ButtonModule, SelectModule, InputTextModule, FormsModule ],
   templateUrl: './create-object.component.html',
-  styleUrl: './create-object.component.scss'
+  styleUrls: ['./create-object.component.scss']
 })
 export class CreateObjectComponent {
+  // List of implemented object types
+  types = ['Cube', 'Sphere', 'Cylinder', 'Pyramid'];
+
+  selectedType: string = '';
+  height: number = 0;
+  width: number = 0;
+  depth: number = 0;
+  name: string = '';
 
 }
